7ddd68cf602d3f7d74c444df26cf92c2eee3f702
Vladi Kuban <vladimir.kuban88@gmail.com>
2022-08-09 15:20:07 +1200

    initial commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,12 @@
+import { TasksService } from './tasks.service';
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+}
