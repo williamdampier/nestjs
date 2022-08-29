6719741c3ea74b5b05a1ee7b0389bb96db033a9c
williamdampier <74879032+williamdampier@users.noreply.github.com>
2022-08-29 15:05:20 +1200

    scratching project

---
+++ src/task/task.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { TaskService } from './task.service';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get()
+  getTasks(): string {
+    return this.taskService.getTasks();
+  }
+}
